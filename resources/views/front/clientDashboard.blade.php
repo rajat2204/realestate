0c746b8ea094306d50c1a37a20b1222e097cf83e
preetiverma123 <preetiverma123>
2019-04-15 13:24:39 +0530

    change

--- resources/views/front/clientDashboard.blade.php
+++ resources/views/front/clientDashboard.blade.php
@@ -23,19 +23,19 @@
                       <table class="table">
                         <tbody>
                           <tr>
-                            <td style="text-align:right;">Name:</td>
+                            <td style="text-align:right;" class="inputBold">Name:</td>
                             <td style="text-align:left;">{{Auth::user()->first_name}}</td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">Registered As:</td>
+                            <td style="text-align:right;" class="inputBold">Registered As:</td>
                             <td style="text-align:left;">{{ucfirst(Auth::user()->user_type)}}</td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">Email:</td>
+                            <td style="text-align:right;" class="inputBold">Email:</td>
                             <td style="text-align:left;">{{Auth::user()->email}}</td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">Mobile no.:</td>
+                            <td style="text-align:right;" class="inputBold">Mobile no.:</td>
                             <td style="text-align:left;">{{Auth::user()->phone}}</td>
                           </tr>
                          </tbody>
@@ -51,15 +51,15 @@
                           <table class="table">
                             <tbody>
                                <tr>
-                                <td style="text-align:right;">Old Password:</td>
+                                <td style="text-align:right;" class="inputBold">Old Password:</td>
                                 <td style="text-align:left;"><input type="password" name="password"></td>
                               </tr>
                               <tr>
-                                <td style="text-align:right;">New Password:</td>
+                                <td style="text-align:right;" class="inputBold">New Password:</td>
                                 <td style="text-align:left;"><input type="password" name="new_password"></td>
                               </tr>
                               <tr>
-                                <td style="text-align:right;">Confirm New Password:</td>
+                                <td style="text-align:right;" class="inputBold">Confirm New Password:</td>
                                 <td style="text-align:left;"><input type="password" name="confirm_password"></td>
                               </tr>
                               <tr>
@@ -83,34 +83,34 @@
                             <td style="text-align:right;"><input type="hidden" name="id" value="{{\Auth::user()->id}}"></td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">Upload Profile:</td>
+                            <td style="text-align:right;" class="inputBold">Upload Profile:</td>
                             <td style="text-align:left;"><input type="file" name="photo" onchange="readURL(this)" id="uploadFile" accept="image/*" style="border:none;"></td>
                             <div>
-                              <img style="max-width: 250px;" src="{{asset('assets/img/avatar.png')}}" id="adminimg" alt="No Featured Image Added">
+                              <img style="max-width: 250px;" src="{{asset('assets/img/avatar.png')}}" id="adminimg" alt="No Featured Image Added" width="100" height="100" class="img-circle border-img">
                             </div>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">Name:</td>
+                            <td style="text-align:right;" class="inputBold">Name:</td>
                             <td style="text-align:left;"><input type="text" name="name" value="{{Auth::user()->first_name}}"></td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">Father's/Mother's Name:</td>
+                            <td style="text-align:right;" class="inputBold">Father's/Mother's Name:</td>
                             <td style="text-align:left;"><input type="text" name="father_name" value=""></td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">Registered As:</td>
+                            <td style="text-align:right;" class="inputBold">Registered As:</td>
                             <td style="text-align:left;"><input type="text" name="user_type" value="{{ucfirst(Auth::user()->user_type)}}" readonly></td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">Occupation:</td>
+                            <td style="text-align:right;" class="inputBold">Occupation:</td>
                             <td style="text-align:left;"><input type="text" name="occupation"></td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">Email:</td>
+                            <td style="text-align:right;" class="inputBold">Email:</td>
                             <td style="text-align:left;"><input type="text" name="email" value="{{Auth::user()->email}}" readonly></td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">Mobile no.:</td>
+                            <td style="text-align:right;" class="inputBold">Mobile no.:</td>
                             <td style="text-align:left;"><input type="text" name="phone" value="{{Auth::user()->phone}}"></td>
                           </tr>
                          </tbody>
@@ -118,27 +118,27 @@
                        <table class="table tableLeft">
                         <tbody>
                           <tr>
-                            <td style="text-align:right;">Address</td>
+                            <td style="text-align:right;" class="inputBold">Address</td>
                             <td style="text-align:left;"><input type="text" name="address" id="autocomplete"></td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">District:</td>
+                            <td style="text-align:right;" class="inputBold">District:</td>
                             <td style="text-align:left;"><input type="text" name="district"></td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">State:</td>
+                            <td style="text-align:right;" class="inputBold">State:</td>
                             <td style="text-align:left;"><input type="text" name="state" value=""></td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">DOB:</td>
+                            <td style="text-align:right;" class="inputBold">DOB:</td>
                             <td style="text-align:left;"><input type="date" name="dob"></td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">PAN Number:</td>
+                            <td style="text-align:right;" class="inputBold">PAN Number:</td>
                             <td style="text-align:left;"><input type="text" name="pan"> </td>
                           </tr>
                           <tr>
-                            <td style="text-align:right;">Nationality:</td>
+                            <td style="text-align:right;" class="inputBold">Nationality:</td>
                             <td style="text-align:left;"><input type="text" name="nationality"></td>
                           </tr>
                           <tr>
