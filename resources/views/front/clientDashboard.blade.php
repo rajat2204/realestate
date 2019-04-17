1fc38700fcfc5057522327a0392be293b2f3b8cf
preetiverma123 <preetiverma123>
2019-04-17 13:05:54 +0530

    changes

--- resources/views/front/clientDashboard.blade.php
+++ resources/views/front/clientDashboard.blade.php
@@ -332,7 +332,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle" style="text-align: center;">Payment Plan</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle" style="text-align: center;color:#1878a0;">Payment Plan</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
