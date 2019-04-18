5fb76b7760a2ec55bef89d25fb02feaee5046ec5
rajat <rajat.igniterpro@gmail.com>
2019-04-18 14:53:40 +0530

    changes

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -23,26 +23,12 @@
             <p>Categories</p>
           </div>
           <div class="icon">
-            <i class="fa fa-fw fa-sitemap"></i>
+            <i class="fa fa-tasks"></i>
           </div>
           <a href="{{url('admin/categories')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <!-- ./col -->
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-green">
-          <div class="inner">
-            <h3>{{ \App\Models\Agents::count() }}</h3>
-            <p>Agents</p>
-          </div>
-          <div class="icon">
-            <i class="fa fa-user-secret"></i>
-          </div>
-          <a href="{{url('admin/agent')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-
       <div class="col-lg-3 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-red">
@@ -70,10 +56,10 @@
           <a href="{{url('admin/project')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
       </div>
-      <!-- ./col -->
+
       <div class="col-lg-3 col-xs-6">
         <!-- small box -->
-        <div class="small-box bg-aqua">
+        <div class="small-box bg-green">
           <div class="inner">
             <h3>{{ \App\Models\Property::count() }}</h3>
             <p>Properties</p>
@@ -87,7 +73,7 @@
 
       <div class="col-lg-3 col-xs-6">
         <!-- small box -->
-        <div class="small-box bg-green">
+        <div class="small-box bg-aqua">
           <div class="inner">
             <h3>{{ \App\Models\Purchase::count() }}</h3>
             <p>Purchases</p>
@@ -103,6 +89,65 @@
         <!-- small box -->
         <div class="small-box bg-red">
           <div class="inner">
+            <h3>{{ \App\Models\Plans::count() }}</h3>
+            <p>Plans</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-briefcase"></i>
+          </div>
+          <a href="{{url('admin/plans')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-yellow">
+          <div class="inner">
+            <h3>{{ \App\Models\Agents::count() }}</h3>
+            <p>Agents</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-user-secret"></i>
+          </div>
+          <a href="{{url('admin/agent')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-green">
+          <div class="inner">
+            <h3>{{ \App\Models\Clients::count() }}</h3>
+            <p>Clients</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-users fa-fw"></i>
+          </div>
+          <a href="{{url('admin/client')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-aqua">
+          <div class="inner">
+            <h3>{{ \App\Models\Deals::count() }}</h3>
+            <p>Deals</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-thumbs-up fa-fw"></i>
+          </div>
+          <a href="{{url('admin/deals')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+
+      <!-- ./col -->
+
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-red">
+          <div class="inner">
             <h3>{{ \App\Models\Sliders::count() }}</h3>
             <p>Sliders</p>
           </div>
@@ -112,10 +157,38 @@
           <a href="{{url('admin/sliders')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
       </div>
-      <!-- ./col -->
+
       <div class="col-lg-3 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-yellow">
+          <div class="inner">
+            <h3>{{ \App\Models\Notice::count() }}</h3>
+            <p>Notice</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-bell"></i>
+          </div>
+          <a href="{{url('admin/notice')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-green">
+          <div class="inner">
+            <h3>{{ \App\Models\SocialMedia::count() }}</h3>
+            <p>Social Media</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-fw fa-linkedin-square"></i>
+          </div>
+          <a href="{{url('admin/social')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-aqua">
           <div class="inner">
             <h3>{{ \App\Models\Testimonials::count() }}</h3>
             <p>Testimonials</p>
@@ -129,7 +202,7 @@
 
       <div class="col-lg-3 col-xs-6">
         <!-- small box -->
-        <div class="small-box bg-aqua">
+        <div class="small-box bg-red">
           <div class="inner">
             <h3>{{ \App\Models\Services::count() }}</h3>
             <p>Services</p>
@@ -141,19 +214,6 @@
         </div>
       </div>
 
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-green">
-          <div class="inner">
-            <h3>{{ \App\Models\Deals::count() }}</h3>
-            <p>Deals</p>
-          </div>
-          <div class="icon">
-            <i class="fa fa-thumbs-up fa-fw"></i>
-          </div>
-          <a href="{{url('admin/deals')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
       <!-- ./col -->
     </div>
   </section>
